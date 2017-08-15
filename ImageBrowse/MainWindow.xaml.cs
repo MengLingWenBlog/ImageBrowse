3c228fb3d7e1c3389d35ce6a0c96ced133d07dc6
shuaili <shuaili7194@163.com>
2017-08-15 16:59:14 +0800

    原始WPF模板工程

--- ImageBrowse/MainWindow.xaml.cs
+++ ImageBrowse/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Forms;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ImageBrowse
 {
@@ -23,6 +16,40 @@ namespace ImageBrowse
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.Windows.MessageBox.Show("文件夹打开成功");
+                var imgs = new ImageGetter().GetList(fbd.SelectedPath);
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("请选择一个文件夹");
+            }
+            //var fileList = Dirctory.EnumerateFile();
+        }
+
+        
+    }
+
+    class ImageGetter
+    {
+        public ImageGetter()
+        {
+
+        }
+
+        public ImageList GetList(string folderName)
+        {
+            var fileList = Directory.EnumerateFiles(folderName);
+            string[] tokenList = { ".jpg", ".png", ".bmp", ".jpeg", "gif" };
+            var imageList = fileList.Where(x => tokenList.Any(x1 => x.EndsWith(x1, StringComparison.InvariantCultureIgnoreCase)));
+            var images1 = imageList.Select(x => new Image{ Source = new BitmapImage(new Uri(x)) });
+            return images1.ToList();
         }
     }
 }
